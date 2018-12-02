c56b8827ac61ee195a9010c86029639588d151d1
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2018-12-02 01:17:20 -0500

    Document USART_init() function

--- include/serial.h
+++ include/serial.h
@@ -1,6 +1,11 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
+/**
+ * Initializes 8N1 Serial communication.
+ * Intended to be used for sending data to a computer for debugging purposes.
+ * After a call to this function, stdin, stdout, and stderr will be set.
+ */
 void USART_init();
 
 #endif /* SERIAL_H */
