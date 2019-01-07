73313c89c2798ab9e513c72f0e40f592d46b3358
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-01-06 21:18:10 -0500

    Port code to atmega32u4

--- src/common/serial.c
+++ src/common/serial.c
@@ -11,36 +11,36 @@ static int USART_putchar(char data, FILE *stream)
         USART_putchar('\r', stream);
     }
     /* wait until transmit data register is empty */
-    loop_until_bit_is_set(UCSR0A, UDRE0);
+    loop_until_bit_is_set(UCSR1A, UDRE1);
     /* set the data register with some data */
-    UDR0 = data;
+    UDR1 = data;
     return 0;
 }
 
 static int USART_getchar(FILE *stream)
 {
     /* wait until there is data in the receive data register */
-    loop_until_bit_is_set(UCSR0A, RXC0);
+    loop_until_bit_is_set(UCSR1A, RXC1);
     /* return data in receive data register */
-    return UDR0;
+    return UDR1;
 }
 
 void USART_init(void)
 {
     /* set baud rate */
-    UBRR0H = UBRRH_VALUE;
-    UBRR0L = UBRRL_VALUE;
+    UBRR1H = UBRRH_VALUE;
+    UBRR1L = UBRRL_VALUE;
 
     #if	USE_2X
         UCSR0A |= _BV(U2X0);
     #else
-        UCSR0A &= ~_BV(U2X0);
+        UCSR1A &= ~_BV(U2X1);
     #endif
 
     /* enable RX and TX */
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0);
+    UCSR1B = _BV(RXEN1) | _BV(TXEN1);
     /* 8 data bits, 1 stop bit, no parity */
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+    UCSR1C = _BV(UCSZ11) | _BV(UCSZ10);
 
     /* opens a stream with write intent and sets stdout and stderr */
     fdevopen(USART_putchar, NULL);
--- src/controller/controller.c
+++ src/controller/controller.c
@@ -5,39 +5,40 @@
 #include "macros.h"
 #include "adc.h"
 
-#define BLINK_DELAY_MS 1000
+#define BLINK_DELAY_MS 500
 
 int main(void)
 {
+    /* Init serial communication */
     USART_init();
     puts("FOOBAR");
 
-    SBI(ADCSRA, ADEN);  /* Enable ADC */
-    SBI(ADCSRA, ADPS0); /**********************************/
-    SBI(ADCSRA, ADPS1); /* Set ADC prescaler value to 128 */
-    SBI(ADCSRA, ADPS2); /**********************************/
-    SBI(ADMUX, REFS0);  /* Use AVcc (~5V) as AREF voltage */
-    CBI(PRR, PRADC);    /* Turn off power reduction to the ADC */
+    // SBI(ADCSRA, ADEN);  /* Enable ADC */
+    // SBI(ADCSRA, ADPS0); /**********************************/
+    // SBI(ADCSRA, ADPS1); /* Set ADC prescaler value to 128 */
+    // SBI(ADCSRA, ADPS2); /**********************************/
+    // SBI(ADMUX, REFS0);  /* Use AVcc (~5V) as AREF voltage */
+    // CBI(PRR, PRADC);    /* Turn off power reduction to the ADC */
 
-    /* set pin 5 of PORTB for output */
-    // SBI(DDRB, DDB5);
+    /* set pin 13 of PORTC for output */
+    SBI(DDRC, DDC7);
 
     while (1) {
-        // /* set pin 5 high to turn led on */
-        // SBI(PORTB, PORTB5);
-        // _delay_ms(BLINK_DELAY_MS);
+        // /* set pin 13 high to turn led on */
+        SBI(PORTC, PORTC7);
+        _delay_ms(BLINK_DELAY_MS);
 
-        // /* set pin 5 low to turn led off */
-        // CBI(PORTB, PORTB5);
-        // _delay_ms(BLINK_DELAY_MS);
+        // /* set pin 13 low to turn led off */
+        CBI(PORTC, PORTC7);
+        _delay_ms(BLINK_DELAY_MS);
 
         /* Select x-axis for reading */
-        unsigned int x_axis = analog_read(A0);
+        // unsigned int x_axis = analog_read(A0);
 
         /* Select y-axis for reading */
-        unsigned int y_axis = analog_read(A1);
+        // unsigned int y_axis = analog_read(A1);
 
-        printf("x_axis = %u, y_axis = %u\n", x_axis, y_axis);
-        _delay_ms(50U);
+        // printf("x_axis = %u, y_axis = %u\n", x_axis, y_axis);
+        // _delay_ms(50U);
     }
 }
